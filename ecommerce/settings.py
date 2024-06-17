ca545b0fad4b6de6d7c49ce19701c54ef70a192e
chebii <ianchebii19@gmail.com>
2024-06-17 08:59:58 +0300

    to br deployed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import django_heroku
 from decouple import config
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -70,7 +71,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
